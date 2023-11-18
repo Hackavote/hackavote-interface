9c03d56ebc37bc875c646256ff2f2086b074744e
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-11-18 14:40:15 +0300

    feat: view project's details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const ProjectCard = ({project}: {
   return (
     <div className="w-full m-10 md:w-80 rounded-xl bg-gray-600 p-5">
       <Link to={'/project/' + project.index} className="flex flex-col items-start">
-        <img className="rounded-t-xl h-3/4 w-full object-cover" alt="project-image"
+        <img className="rounded-t-xl h-3/4 w-full object-cover" alt="project"
              src='/img/project-image-placeholder.png'/>
         <div className="text-font-color my-2">A short description about the project</div>
         <button className="bg-blue-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
--- src/pages/project/_id.tsx
+++ src/pages/project/_id.tsx
@@ -1,25 +1,49 @@
 import {useParams} from "react-router-dom";
 import useHackavoteProject from "hooks/useHackavoteProject";
+import Spinner from "components/Spinner";
+import {AddressZero} from "constants/index";
+import {copyToClipboard} from "utils/copyToClipboard";
 
 const Project = () => {
   const {projectId} = useParams()
   const {project} = useHackavoteProject(projectId)
-  return (
-    <div>
-      {JSON.stringify(project)}
-      Technicality - What is the complexity of problem being addressed, or their approach to solving it?
-
-      Originality - Is the project tackling a new or unsolved problem, or creating unique/creative solution to an
-      existing problem?
-
-      Practicality - How complete/functional is the project? Is it ready to be used by their intended audience?
-
-      Usability (UI/UX/DX) - Is the project easy to use? Has the team made good effort in removing friction for the
-      user?
-
-      WOW factor - Catch-all for other factors the previous categories may have missed
+  return project ? (
+    <div className="p-10">
+      <div className="flex flex-wrap">
+        <img alt="project" className="w-2/3 m-auto"
+             src='/img/project-image-placeholder.png'/>
+        <div className="min-w-[400px] w-1/3 p-5 flex-col">
+          <div>
+            <p className="text-center font-bold text-3xl">Project Title</p>
+            <p className="py-2">
+              A short description about the project
+            </p>
+          </div>
+          <div className="pt-5">
+            <p>Eth Global submission</p>
+            <a href={project.submissionInfoUrl}
+               className="project-meta-value">{project.submissionInfoUrl}</a>
+          </div>
+          <div>
+            <p>Contact and Social Media</p>
+            {project.socialMediaUrl ? <a className="project-meta-value">{project.socialMediaUrl}</a> : <p
+              className="project-meta-value">Not provided yet</p>}
+          </div>
+          <div>
+            <p>Donation Address</p>
+            <p
+              onClick={() => {
+                if (project && project.donationAddress !== AddressZero) {
+                  copyToClipboard(project.donationAddress)
+                  alert('Copied!')
+                }
+              }}
+              className={`project-meta-value ${project.donationAddress !== AddressZero ? 'cursor-pointer' : ''}`}>{project.donationAddress === AddressZero ? 'Not provided yet' : project.donationAddress}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  ) : <Spinner/>;
 };
 
 export default Project; /* Rectangle 18 */
