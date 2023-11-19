ff6cacd7ae4f328753fd8f03644896c50e49da57
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-11-19 07:53:59 +0300

    feat: new hackavote contract

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -6,5 +6,5 @@ export const HACKAVOTE_PROJECTS_CONTRACT_ADDRESS_MAP: AddressMap = {
   [gnosis.id]: (process.env.REACT_APP_HACKAVOTE_PROJECTS_CONTRACT_ADDRESS as Address | undefined) || '0x2aBD91e1c130c54996Df39854D3c3cf357636f5A'
 }
 export const HACKAVOTE_CONTRACT_ADDRESS_MAP: AddressMap = {
-  [gnosis.id]: (process.env.REACT_APP_HACKAVOTE_CONTRACT_ADDRESS as Address | undefined) || '0x40E1EA4B9d149CAEf0C239A49E967EeB13681749'
+  [gnosis.id]: (process.env.REACT_APP_HACKAVOTE_CONTRACT_ADDRESS as Address | undefined) || '0xfb69121cc34B33186F4C4C9b7061D8D67f50Bc15'
 }
